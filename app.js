3485fd0cf2c4c9171d8551a657cc85e8b1160509
srujanjill <srujanjilla15@gmail.com>
2020-09-04 22:23:07 +0530

    Real-time database removing getting the data section

--- app.js
+++ app.js
@@ -46,15 +46,15 @@ function renderCafe(doc) {
   });
 }
 
-// getting data
-db.collection("cafes")
-  .orderBy("city")
-  .get()
-  .then((snapshot) => {
-    snapshot.docs.forEach((doc) => {
-      renderCafe(doc);
-    });
-  });
+// // getting data
+// db.collection("cafes")
+//   .orderBy("city")
+//   .get()
+//   .then((snapshot) => {
+//     snapshot.docs.forEach((doc) => {
+//       renderCafe(doc);
+//     });
+//   });
 
 // saving data
 form.addEventListener("submit", (e) => {
@@ -65,4 +65,18 @@ form.addEventListener("submit", (e) => {
   });
   form.name.value = "";
   form.city.value = "";
+});
+
+// real-time listener & get data
+db.collection('cafes').orderBy('city').onSnapshot(snapshot => {
+  let changes = snapshot.docChanges();
+  changes.forEach(change => {
+      console.log(change.doc.data());
+      if(change.type == 'added'){
+          renderCafe(change.doc);
+      } else if (change.type == 'removed'){
+          let li = cafeList.querySelector('[data-id=' + change.doc.id + ']');
+          cafeList.removeChild(li);
+      }
+  });
 });
